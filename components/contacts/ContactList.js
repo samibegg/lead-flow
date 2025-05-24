42b9855b5000c47c66b8f56008d4ed7abe0209f4
samibegg <sami.begg@mongodb.com>
2025-05-23 23:32:09 -0400

    btn

--- components/contacts/ContactList.js
+++ components/contacts/ContactList.js
@@ -41,14 +41,14 @@ export default function ContactList({ contacts, totalItems, currentPage, totalPa
             <button
               onClick={handlePreviousPage}
               disabled={currentPage <= 1}
-              className="px-4 py-2 bg-primary hover:bg-primary-hover-light dark:bg-primary-dark dark:hover:bg-primary-hover-dark text-white text-sm font-medium rounded-md disabled:bg-slate-300 dark:disabled:bg-slate-600 disabled:cursor-not-allowed transition-colors"
+              className="px-4 py-2 bg-primary hover:bg-primary-hover-light dark:bg-primary-dark dark:hover:bg-primary-hover-dark text-black text-sm font-medium rounded-md disabled:bg-slate-300 dark:disabled:bg-slate-600 disabled:cursor-not-allowed transition-colors"
             >
               Previous
             </button>
             <button
               onClick={handleNextPage}
               disabled={currentPage >= totalPages}
-              className="px-4 py-2 bg-primary hover:bg-primary-hover-light dark:bg-primary-dark dark:hover:bg-primary-hover-dark text-white text-sm font-medium rounded-md disabled:bg-slate-300 dark:disabled:bg-slate-600 disabled:cursor-not-allowed transition-colors"
+              className="px-4 py-2 bg-primary hover:bg-primary-hover-light dark:bg-primary-dark dark:hover:bg-primary-hover-dark text-black text-sm font-medium rounded-md disabled:bg-slate-300 dark:disabled:bg-slate-600 disabled:cursor-not-allowed transition-colors"
             >
               Next
             </button>
